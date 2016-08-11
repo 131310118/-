a311f033eae0f75499e7ab44dbfa6ecfb87b8ab6
wangjun <302949537@qq.com>
2016-08-11 14:33:01 +0800

    分页

--- king/page.js
+++ king/page.js
@@ -13,53 +13,144 @@ function page(element,option){
         pages:1,
         onPage:1,
         node:6,
-        href:"javascript:void(0);"
+        href:"javascript:void(0);",
+        click:function(){}
     });
     config.hrefIsFun = (typeof config.href == "function");
     var cl = tag('ul','cl');
     if(config.page){
         var p = cl.append('li');
-        if(config.pages<=node){
-            for(var i=1;i<=node;i++){
+        if(config.pages<=config.node){
+            for(var i=1;i<=config.node;i++){
                 if(config.onPage==i){
                     p.append('a',{
                         href:config.hrefIsFun?config.href(i):config.href,
                         innerHTML:i,
-                        class:'on'
-                    })
+                        className:'on'
+                    });
                 }else{
                     p.append('a',{
                         href:config.hrefIsFun?config.href(i):config.href,
                         innerHTML:i
-                    })
+                    }).addEventListener('click',function(){
+                        config.onPage = +this.innerHTML.trim();
+                        page(element,config);
+                        config.click(config.onPage);
+                    });
                 }
             }
         }else{
             var half = Math.floor(config.node/2);
             if(config.onPage>half){
-                for(var i=config.onPage-half;i<=config.onPage;i++){
+                var lessRight = half+config.onPage-config.pages;
+                for(var i=config.onPage-half-(lessRight>0?lessRight:0);i<config.onPage;i++){
+                    p.append('a',{
+                        href:config.hrefIsFun?config.href(i):config.href,
+                        innerHTML:(i==config.onPage-half-(lessRight>0?lessRight:0)&&i!=1&&config.first?'1..':i)
+                    }).addEventListener('click',function(){
+                        config.onPage = +this.innerHTML.trim().match(/(\d)+/)[0];
+                        page(element,config);
+                        config.click(config.onPage);
+                    });
+                }
+            }else{
+                for(var i=1;i<config.onPage;i++){
                     p.append('a',{
                         href:config.hrefIsFun?config.href(i):config.href,
                         innerHTML:i
-                    })
+                    }).addEventListener('click',function(){
+                        config.onPage = +this.innerHTML.trim();
+                        page(element,config);
+                        config.click(config.onPage);
+                    });
+                }
+            };
+            p.append('a',{
+                href:config.hrefIsFun?config.href(i):config.href,
+                innerHTML:config.onPage,
+                className:'on'
+            });
+            if(config.onPage<=(config.pages-half)){
+                for(var i=+config.onPage+ 1,j = +config.onPage+half-(config.onPage-half>0?0:config.onPage-half);i<=j;i++){
+                    p.append('a',{
+                        href:config.hrefIsFun?config.href(i):config.href,
+                        innerHTML:(i==j&&i!=config.pages&&config.last?config.pages+'..':i)
+                    }).addEventListener('click',function(){
+                        config.onPage = +this.innerHTML.trim().match(/(\d)+/)[0];
+                        page(element,config);
+                        config.click(config.onPage);
+                    });
+                }
+            }else{
+                for(var i=+config.onPage+1;i<=config.pages;i++){
+                    p.append('a',{
+                        href:config.hrefIsFun?config.href(i):config.href,
+                        innerHTML:i
+                    }).addEventListener('click',function(){
+                        config.onPage = +this.innerHTML.trim();
+                        page(element,config);
+                        config.click(config.onPage);
+                    });
                 }
             }
         }
     }
     if(config.jump){
         cl.append('li','form_page').append('input',{
-            class:'page_input',
+            className:'page_input',
             type:'text'
+        }).addEventListener('keydown',function(e){
+            if (e.keyCode == 13 || e.keyCode == 108) {
+                config.onPage = (+this.value.trim())>config.pages?config.pages:(+this.value.trim()<1?1:+this.value.trim());
+                page(element,config);
+                config.click(config.onPage);
+            }
         })
     }
     if(config.next){
-        cl.append('li','next').append('a',{
-
-        })
+        if(config.onPage!=config.pages){
+            var next = cl.append('li','next');
+            next.append('a',{
+                href:config.hrefIsFun?config.href(+config.onPage+1):config.href,
+                innerHTML:'<span>下一页</span><i class="fa fa-chevron-right"></i>'
+            });
+            next.addEventListener('click',function(){
+                config.onPage = (+config.onPage+1);
+                page(element,config);
+                config.click(config.onPage);
+            });
+        }
+    }
+    if(config.prev){
+        if(config.onPage!=1){
+            var pre = cl.append('li','pre');
+            pre.append('a',{
+                href:config.hrefIsFun?config.href(config.onPage-1):config.href,
+                innerHTML:'<i class="fa fa-chevron-left"></i>'
+            });
+            pre.addEventListener('click',function(){
+                config.onPage = (config.onPage-1);
+                page(element,config);
+                config.click(config.onPage);
+            });
+        }
+    }
+    element.innerHTML = '';
+    element.appendChild(cl);
+    function Next(){
+        next&&next.click();
+    }
+    function Prev(){
+        pre&&pre.click();
+    }
+    function pageTo(page){
+        config.onPage = page;
+        page(element,config);
     }
     return {
-        next:next,
-        prev:prev,
+        next:Next,
+        prev:Prev,
+        pageTo:pageTo
     }
 }
 
